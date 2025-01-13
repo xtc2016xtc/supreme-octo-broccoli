3a20d551b9225e23a73fcc3a3b3a47f78e005231
Admin Bear <728611827@qq.com>
2025-01-13 04:42:26 +0000

    初始化

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+
+const App = () => {
+  return (
+    <div className="">
+      <Button className='bg-black text-white'>Click me</Button>
+    </div>
+  )
+}
+
+export default App
